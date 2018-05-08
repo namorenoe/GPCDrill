440b64b57bf988163df754032e5b9f97100fab9d
jmalovera10 <jma.lovera10@uniandes.edu.co>
2018-05-08 16:09:18 -0500

    register create

---
+++ imports/ui/registration/Registration.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import PropTypes from "prop-types";
+
+import "./Registration.css";
+
+class Registration extends Component {
+    render(){
+        
+    }
+}
+
+Registration.propTypes = {
+
+};
