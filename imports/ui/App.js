07e17749acc791e9a32af25e6ac5035794e362db
namorenoe <35923470+namorenoe@users.noreply.github.com>
2021-07-23 19:15:47 -0500

    Update App.js

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -22,7 +22,7 @@ const esta = {
 }
 
 //cedulas permitidas
-const cedulasString = "1022434817, 1022434818, 1022434819";
+const cedulasString = "1022434817,1022434818,1022434819";
 const cedulas = cedulasString.split(',');
 
 class App extends Component {
